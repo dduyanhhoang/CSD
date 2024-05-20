ba97628118ed0fa265526564640a5ab780849c99
dduyanhhoang <dduyanhhoang@gmail.com>
2024-05-20 08:15:23 +0700

    Update array ds

--- data_structures/array/array.py
+++ data_structures/array/array.py
@@ -9,16 +9,6 @@ class Array:
 		self._array = self._make_array(self.capacity)
 
 	@property
-	def capacity(self):
-		return self._capacity
-	
-	@capacity.setter
-	def capacity(self, value: int):
-		if not (0 < value):
-			raise ValueError("Invalid capacity")
-		self._capacity = value
-
-	@property
 	def index(self):
 		return self._index
 
@@ -27,22 +17,58 @@ class Array:
 		self._index = value
 
 	@property
-	def array(self):
-		return self._array
-
-	def _make_array(self, capacity: int):
-		return (capacity * ctypes.py_object)()
+	def capacity(self):
+		return self._capacity
+	
+	@capacity.setter
+	def capacity(self, value: int):
+		if not (isinstance(value, int) and 0 < value):
+			raise ValueError("Invalid capacity")
+		self._capacity = value
 
 	def __len__(self) -> int:
 		return self.capacity
 
 	def __getitem__(self, index):
-		if not (0 <= index <= self.capacity):
+		if not (isinstance(index, int) and 0 <= index < self.index):
 			raise IndexError('invalid index')
 		return self._array[index]
 
-	def insert(self, obj):
+	def _make_array(self, capacity: int):
+		return (capacity * ctypes.py_object)()
+
+	def insert(self, index: int, value):
 		if self.index == self.capacity:
 			raise CapacityReachedError()
+
+		if not (isinstance(index, int) and 0 <= index <= self.index):
+			raise IndexError('invalid index')
+
 		self._array[self.index] = obj
 		self.index += 1
+
+	def remove_by_value(self, value) -> None:
+		for k in range(self.index):
+			if self._array[i] == value:
+				self.remove_by_index()
+				return
+		raise ValueError('value not found')
+
+	def remove_by_index(self, index: int) -> None:
+		if not (isinstance(index, int) and 0 <= index <= self.index):
+			raise IndexError('invalid index')
+
+		for i in range(index, self.index - 1):
+			self._array[i] = self._array[i + 1]
+
+		self._array[self.index - 1] = None
+		self._index -= 1
+
+	def __str__(self) -> str:
+		darr_str = "["
+		for i in range(self.index):
+			darr_str += f" {self._array[i]},"
+		darr_str = darr_str.rstrip(',')
+		darr_str += " ]"
+		return darr_str
+		
--- tests/test_data_structure/test_array/test_array.py
+++ tests/test_data_structure/test_array/test_array.py
@@ -7,16 +7,3 @@ def test_initialization():
     arr = Array(10)
     assert len(arr) == 10
     assert arr.capacity == 10
-
-def test_array_insert():
-    array = Array(3)
-    array.insert(1)
-    array.insert(2)
-    array.insert(3)
-
-    assert array[0] == 1
-    assert array[1] == 2
-    assert array[2] == 3
-
-    with pytest.raises(CapacityReachedError):
-        array.insert(4)
