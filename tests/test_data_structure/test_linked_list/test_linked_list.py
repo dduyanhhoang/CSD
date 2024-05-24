7148f9cb5135771728254c1d692efbd8314cc718
dduyanhhoang <dduyanhhoang@gmail.com>
2024-05-24 14:35:27 +0700

    test: small tests for LinkedList.insert_first

--- tests/test_data_structure/test_linked_list/test_linked_list.py
+++ tests/test_data_structure/test_linked_list/test_linked_list.py
@@ -1,2 +1,14 @@
 import pytest
-from data_structures.linked_list import LinkedList
+from data_structures.linked_list import LinkedList, Node
+
+def test_init():
+	ll = LinkedList()
+	assert len(ll) == 0
+	assert ll.head is None
+
+
+def test_insert_first():
+	ll = LinkedList()
+	ll.insert_first(0)
+	assert len(ll) == 1
+	assert ll._head.data == 0
