b412866e82c5d308a1ad0fc9657418ebe2aa188a
dduyanhhoang <dduyanhhoang@gmail.com>
2024-05-24 14:34:46 +0700

    fix: rewrite LinkedList.insert_first and add minor hints, validation

--- data_structures/linked_list/linked_list.py
+++ data_structures/linked_list/linked_list.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from .node import Node
 
 
@@ -6,86 +7,30 @@ class LinkedList:
         self._head = None
         self._size = 0
 
-    def insert_first(self, value):
-        self._head = Node(value, self._head)
+    @property
+    def size(self) -> int:
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if value < 0:
+            raise ValueError('linked list size could not be negative')
+        self._size = value
+
+    @property
+    def head(self) -> Optional['Node']:
+        return self._head
+
+    @head.setter
+    def head(self, target_node: Optional['Node']) -> None:
+        if not (target_node is None or isinstance(target_node, Node)):
+            raise TypeError('head must be a Node or None')
+        self._head = target_node
+    
+    def __len__(self):
+        return self.size
+
+    def insert_first(self, value) -> None:
+        new_first = Node(value, self._head)
+        self._head = new_first
         self._size += 1
-
-    def insert_last(self, value):
-        if self._size == 0:
-            self.insert_first(value)
-        else:
-            last_node = self._head
-            while last_node.next:
-                last_node = last_node.next
-            last_node.next = Node(value)
-            self._size += 1
-
-    def insert_after_node(self, target_node, value):
-        if self._size == 0:
-            raise Exception('linked list is empty')
-        else:
-            current_node = self._head
-            while current_node and current_node != target_node:
-                current_node = current_node.next
-            if current_node is None:
-                raise Exception('Node not found in the list')
-            new_node = Node(value, current_node.next)
-            current_node.next = new_node
-            self._size += 1
-
-    def delete_first(self):
-        if self._size == 0:
-            raise Exception('linked list is empty')
-
-        old_head_node = self._head
-        self._head = self._head.next
-
-        old_head_node.next = None
-        self._size -= 1
-
-    def delete_after_node(self, target_node):
-        if self._size == 0:
-            raise Exception('linked list is empty')
-        current_node = self._head
-        while current_node and current_node != target_node:
-            current_node = current_node.next
-        if current_node is None or current_node.next is None:
-            raise Exception('Node not found or has no next node')
-        current_node.next = current_node.next.next
-        self._size -= 1
-
-    def delete_last(self):
-        if self._size == 0:
-            raise Exception('linked list is empty')
-        if self._size == 1:
-            self._head = None
-        else:
-            current_node = self._head
-            while current_node.next.next:
-                current_node = current_node.next
-            current_node.next = None
-        self._size -= 1
-
-    def update(self, target_node, value):
-        current_node = self._head
-        while current_node and current_node != target_node:
-            current_node = current_node.next
-        if current_node is None:
-            raise Exception('Node not found in the list')
-        current_node.value = value
-
-    def search(self, value):
-        current_node = self._head
-        while current_node:
-            if current_node.value == value:
-                return current_node
-            current_node = current_node.next
-        return None
-
-    def __str__(self):
-        nodes = []
-        current_node = self._head
-        while current_node:
-            nodes.append(str(current_node.value))
-            current_node = current_node.next
-        return " -> ".join(nodes)
