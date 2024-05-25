7bdae8f208a84ac0291e840cbf00b0b9273933c9
dduyanhhoang <dduyanhhoang@gmail.com>
2024-05-25 09:51:26 +0700

    feat: update NodeNotFoundError message

--- data_structures/exceptions/node_not_found_error.py
+++ data_structures/exceptions/node_not_found_error.py
@@ -1,4 +1,4 @@
 class NodeNotFoundError(Exception):
-	def __init__(self, message="Node not found"):
+	def __init__(self, message="Node does not exist in the data structure"):
 		self.message = message
 		super().__init__(self.message)
